fae20bdb0f8a8cf407a75f774e50ff007f3f77c9
Sendyxls <miskakocev8351@gmail.com>
2025-02-10 12:09:38 +1000

    //

---
+++ Cezar/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Encrypt(string input, int shift = 3)
+    {
+        ViewBag.Original = input;
+        ViewBag.Encrypted = CaesarCipher(input, shift);
+        return View("Index");
+    }
+
+    private string CaesarCipher(string text, int shift)
+    {
+        char[] buffer = text.ToCharArray();
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            char letter = buffer[i];
+            if (char.IsLetter(letter))
+            {
+                char d = char.IsUpper(letter) ? 'A' : 'a';
+                buffer[i] = (char)((letter + shift - d) % 26 + d);
+            }
+        }
+        return new string(buffer);
+    }
+}
